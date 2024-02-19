407403747b165d996c39f3a72895221a4ca0816b
Diego Faria <diegoafbrito12@gmail.com>
2024-02-19 14:34:10 -0400

    código inicial creado

---
+++ celery_test/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'celery_test.settings')
+
+app = Celery('celery_test')
+app.conf.enable_utc = False
+
+app.conf.update(timezone = 'America/Caracas')
+
+app.config_from_object(settings, namespace="CELERY")
+
+# CELERY BEAT SETTINGS
+app.conf.beat_schedule = {
+    
+}
+app.autodiscover_tasks()
+
+@app.task(bind = True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
